42c80821f7e69baea1514d05f3ef9409307725fb
ZharithBedoya <ZharithBedoya>
2025-09-02 19:19:39 -0500

    Ejercicio7-Combinador-de-Listas-con-zip.py ingresar el nombre y la nota del estudiante en listas y unificar en un diccionario.

--- Ejercicio7-Combinador-de-Listas-con-zip.py
+++ Ejercicio7-Combinador-de-Listas-con-zip.py
@@ -5,26 +5,34 @@ notas finales, crea una función que las combine para generar un diccionario. La
 "El estudiante [Nombre] tiene una nota de [Nota]".
 Conceptos aplicados: Funciones, zip(), dict(), bucle for sobre diccionarios.
 '''
+
+
 nom_estu = []
 nota_estudiante = []
 
 print("🗒️==Notas de Estudiantes==🗒️")
 
+cant_st = input("Digite la cantidad de estudiantes que desea ingresar: ")
 
-def main():
-    cant = int(input("Digite la cantidad de estudiantes que desea ingresar: "))
+if not cant_st.isdigit():
+    print("Cantidad debe ser un número")
+    exit()  # Finaliza el programa aquí
+else:
+    cant = int(cant_st)
 
-    for i in range(cant): #en un rango definido por el que ingrese el usuario cant
-        nombre = input(f"Ingrese el nombre de estudiante {i + 1}: ")
-        nota = input(f"Ingrese la nota de estudiante {i + 1}: ")
-        nom_estu.append(nombre) #agregar a num_estu el nombre
-        nota_estudiante.append(nota) #agregar a nota_estudiante la nota
+for i in range(cant):  # en un rango definido por el que ingrese el usuario cant
+    nombre = input(f"Ingrese el nombre de estudiante {i + 1}: ")
+    while not nombre.isalpha(): #mientras que lka cant no sea numerica
+        print("Nombre solo debe contenter letras")
+        nombre=input("Ingrese nuevamente el nombre del estudiante: ")
+    nota = input(f"Ingrese la nota del estudiante {nombre}: ")
+    while not nota.isdigit(): #mientras que la nota no sea numerica
+        print("La nota debe ser numeros")
+        nota = input(f"Ingrese nuevamente la nota del estudiante {nombre}: ")
 
-        estudiante = dict(zip(nom_estu, nota_estudiante)) #unir las dos listas en un diccionario
+    nom_estu.append(nombre)  # agregar a num_estu el nombre
+    nota_estudiante.append(nota)  # agregar a nota_estudiante la nota
+    estudiante = dict(zip(nom_estu, nota_estudiante))  # unir las dos listas en un diccionario
 
-    for estudiante, nota in estudiante.items(): #para estudiantes agregar la nota como un iten a la lista estudiante
-        print(f"El estudiante {nom_estu} tiene una nota de {nota_estudiante}")
-
-
-if __name__ == '__main__':
-    main()
+    for estudiante, nota in estudiante.items():  # recorre las claves y los valores al mismo tiempo
+       print(f"El estudiante {estudiante} tiene una nota de {nota}")
